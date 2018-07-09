9f5645d40d5c4060bc97601cd5ba8ecf0a4cb5a8
Tom McLaughlin <pyro777@gmail.com>
2018-07-09 04:55:03 -0700

    Better tree shaking

--- ts/MatrixModal.tsx
+++ ts/MatrixModal.tsx
@@ -1,8 +1,11 @@
 
 import * as React from "react";
 
-import { Dialog, DialogActions, DialogContent, DialogTitle } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
 import FormControl from "@material-ui/core/FormControl";
 import MenuItem from "@material-ui/core/MenuItem";
 import Select from "@material-ui/core/Select";
